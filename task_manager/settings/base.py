a6033cbad791f6785477743689090c4f3acc161c
Arsen Myroniuk <arisenmyr@gmail.com>
2025-02-06 20:22:48 +0200

    Added features for deployment

--- task_manager/settings/base.py
+++ task_manager/settings/base.py
@@ -94,11 +94,11 @@ USE_TZ = True
 
 
 
-STATIC_URL = 'static/'
-
-STATIC_ROOT = "staticfiles/"
+STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = "staticfiles/"
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
